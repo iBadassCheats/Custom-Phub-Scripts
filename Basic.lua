fb2a09805b6ea6f8bdd51c3beccbf95b946028ae
Jan <149733150+iBadassCheats@users.noreply.github.com>
2025-06-23 00:09:31 +0200

    Update Basic.lua

--- Basic.lua
+++ Basic.lua
@@ -1,6 +1,10 @@
-c_vector2() -- everythings with vector2
-c_vector3() -- everythings with vector3
+c_vector2() -- everythings with vector2 // vector
+c_vector3() -- everythings with vector3 // vector
 
-c_getrankt() -- return c_getrankt()
-c_sleep() -- c_sleep(5)
-c_openprocess() -- return c_openprocess("process")
+c_getrankt() -- return c_getrankt() // string
+c_sleep() -- c_sleep(5) // int
+c_openprocess() -- return c_openprocess("process") // string
+
+c_writememory() -- writememory("addr", "type", "value") // bool
+c_writememory() -- return readmemory("addr", "type") // object
+c_writehook() -- return c_writehook("addr", "bytes", "asm", "name") // bool
