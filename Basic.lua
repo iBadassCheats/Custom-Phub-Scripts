714f32658a33414e2e6f9d1a35bb83d420bcd57e
Jan <149733150+iBadassCheats@users.noreply.github.com>
2025-06-23 00:07:03 +0200

    Create Basic.lua

---
+++ Basic.lua
@@ -0,0 +1,6 @@
+c_vector2() -- everythings with vector2
+c_vector3() -- everythings with vector3
+
+c_getrankt() -- return c_getrankt()
+c_sleep() -- c_sleep(5)
+c_openprocess() -- return c_openprocess("process")
