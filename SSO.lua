acf55e8718d8f6805f3643229be8f97afd72a573
Jan <149733150+iBadassCheats@users.noreply.github.com>
2025-06-23 00:03:22 +0200

    Create SSO.lua

---
+++ SSO.lua
@@ -0,0 +1,13 @@
+j_doublespeed() -- j_doublespeed(false / true) // void
+
+j_nofall() -- j_nofall(false / true) // void
+j_noreset() -- j_noreset(false / true) // void
+
+j_fullquestcomplete() -- j_noreset(false / true) // void
+j_endlessquest() -- j_noreset(false / true) // void
+j_instantdialog() -- j_noreset(false / true) // void
+
+j_getcoord() -- return j_getcoord() // vector
+j_setcoord() -- j_setcoord(Vector3(123.0, 456.0, 789.0)) // bool
+
+j_modding() -- j_modding("global/..") // bool
