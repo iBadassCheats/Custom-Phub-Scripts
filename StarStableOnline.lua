5147cff1814fcb1ca0e7eef80c1f2ebcd2e89989
Jan <149733150+iBadassCheats@users.noreply.github.com>
2025-06-23 01:44:23 +0200

    Update StarStableOnline.lua

--- StarStableOnline.lua
+++ StarStableOnline.lua
@@ -11,3 +11,8 @@ j_getcoord() -- return j_getcoord() // vector
 j_setcoord() -- j_setcoord(Vector3(123.0, 456.0, 789.0)) // bool
 
 j_modding() -- j_modding("global/..") // bool
+j_additem() -- j_additem(6764 / id, 99 / amount) // bool
+
+-- special:
+j_setstarcoins() -- j_setstarcoins(6764, "server side / temp side") // bool
+j_startunban() -- j_startunban("game path", username, password) // bool
