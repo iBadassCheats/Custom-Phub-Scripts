a729c81f384cc34549530e67db7125ee5f2d2ec4
Jan <149733150+iBadassCheats@users.noreply.github.com>
2025-06-23 01:42:31 +0200

    Create GenshinImpact.lua

---
+++ GenshinImpact.lua
@@ -0,0 +1,9 @@
+j_doubleattack() -- j_doublespeed(false / true) // void
+
+j_nofall() -- j_nofall(false / true) // void
+
+j_getcoord() -- return j_getcoord() // vector
+j_setcoord() -- j_setcoord(Vector3(123.0, 456.0, 789.0)) // bool
+
+-- special:
+j_additem() -- j_additem(6764 / id, 99 / amount) // bool
