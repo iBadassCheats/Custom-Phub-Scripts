cc31ee821a5a9f16992ced171b22e436926c1ffe
Jan <149733150+iBadassCheats@users.noreply.github.com>
2025-06-23 01:35:25 +0200

    Create WorldOfWarcraft.lua

---
+++ WorldOfWarcraft.lua
@@ -0,0 +1,6 @@
+j_doublespeed() -- j_doublespeed(false / true) // void
+
+j_nofall() -- j_nofall(false / true) // void
+
+j_getcoord() -- return j_getcoord() // vector
+j_setcoord() -- j_setcoord(Vector3(123.0, 456.0, 789.0)) // bool
