c88b2279b261e89401150ed90d5f1415d0c4276d
Jan <149733150+iBadassCheats@users.noreply.github.com>
2025-06-25 10:27:20 +0200

    Update Basic.lua

--- Basic.lua
+++ Basic.lua
@@ -9,7 +9,7 @@ c_getrankt() -- return c_getrankt() // string
 c_sleep() -- c_sleep(5) // void
 c_openproc() -- return c_openproc("process") // string
 
-c_writeproc() -- c_writeproc("addr", "type", "value") // bool
-c_readproc() -- return c_readproc("addr", "type") // object
+c_writeproc() -- c_writeproc("addr", "int, float, long, ulong, string, double, byte, 2byte", "value") // bool
+c_readproc() -- return c_readproc("addr", "int, float, long, ulong, string, double, byte, 2byte") // object
 c_hookproc() -- return c_hookproc("addr", "bytes", "asm", "name") // bool
 c_aobproc() -- return c_aobproc(0x000000000, 0x7ffffffff "bytes", 0x0) // string
